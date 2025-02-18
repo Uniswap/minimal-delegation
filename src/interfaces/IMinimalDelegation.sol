7d44822dbdc9e7d8919d1bef1e917e933778ee55
Sara Reynolds <snreynolds2506@gmail.com>
2025-02-18 15:47:25 -0500

    support 1271 interface

--- src/interfaces/IMinimalDelegation.sol
+++ src/interfaces/IMinimalDelegation.sol
@@ -5,12 +5,14 @@ import {IAccount} from "account-abstraction/interfaces/IAccount.sol";
 import {IKeyManagement} from "./IKeyManagement.sol";
 import {IERC7821} from "./IERC7821.sol";
 import {IEIP712} from "./IEIP712.sol";
+import {IERC1271} from "@openzeppelin/contracts/interfaces/IERC1271.sol";
 
-/// A non-upgradeable contract that can be delegated to with a 7702 delegation transasction.
+/// A non-upgradeable contract that can be delegated to with a 7702 delegation transaction.
 /// This implementation supports:
 /// ERC-4337 relayable userOps
 /// ERC-7821 batched actions
 /// EIP-712 typed data signature verification
 /// ERC-7201 compliant storage use
+/// ERC-1271 compliant signature verification
 /// Alternative key management and verification
-interface IMinimalDelegation is IKeyManagement, IAccount, IERC7821, IEIP712 {}
+interface IMinimalDelegation is IKeyManagement, IAccount, IERC7821, IEIP712, IERC1271 {}
