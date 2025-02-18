10ab9cfb0f0d76ba29fcce24d12281efa1d38986
Eric Zhong <eric.zhong@uniswap.org>
2025-02-18 18:34:32 -0500

    Add comments

--- src/lib/MinimalDelegationStorageLib.sol
+++ src/lib/MinimalDelegationStorageLib.sol
@@ -6,6 +6,7 @@ library MinimalDelegationStorageLib {
         mapping(bytes32 keyHash => bytes encodedKey) keyStorage;
     }
 
+    /// @dev keccak256(abi.encode(uint256(keccak256("Uniswap.MinimalDelegation")) - 1)) & ~bytes32(uint256(0xff))
     bytes32 private constant MINIMAL_DELEGATION_STORAGE_LOCATION =
         0x21f3d48e9724698d61a2dadd352c365013ee5d0f841f7fc54fb8a78301ee0c00;
 
