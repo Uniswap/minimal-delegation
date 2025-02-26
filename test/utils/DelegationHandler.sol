a43668e200c59544f0911de7518c71bd2b4efc5a
dianakocsis <diana.kocsis@uniswap.org>
2025-02-26 15:33:21 -0500

    comment for future

--- test/utils/DelegationHandler.sol
+++ test/utils/DelegationHandler.sol
@@ -16,6 +16,7 @@ contract DelegationHandler is Test {
     Key public mockSecp256k1Key = Key(0, KeyType.Secp256k1, true, abi.encodePacked(mockSecp256k1PublicKey));
 
     address mockSecp256k1PublicKey2 = makeAddr("mockSecp256k1PublicKey2");
+    // May need to remove block.timestamp in the future if using vm.roll / warp
     Key public mockSecp256k1Key2 =
         Key(uint40(block.timestamp + 3600), KeyType.Secp256k1, false, abi.encodePacked(mockSecp256k1PublicKey2));
 
