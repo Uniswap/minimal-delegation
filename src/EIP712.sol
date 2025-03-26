b71b8e00f68f2c16a4c55ac8683e7d3e829e8b4b
Eric Zhong <ezhong1900@gmail.com>
2025-03-26 11:41:26 -0400

    standrize OZ imports (#45)

--- src/EIP712.sol
+++ src/EIP712.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.23;
 
-import {IERC5267} from "openzeppelin-contracts/contracts/interfaces/IERC5267.sol";
+import {IERC5267} from "@openzeppelin/contracts/interfaces/IERC5267.sol";
 import {IEIP712} from "./interfaces/IEIP712.sol";
 
 /// @title EIP712
