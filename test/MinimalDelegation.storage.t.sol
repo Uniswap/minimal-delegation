63c4bba766f8c6ddc178ebc8f778c3dfaadc02eb
Eric Zhong <eric.zhong@uniswap.org>
2025-04-03 18:49:11 -0400

    Add test

--- test/MinimalDelegation.storage.t.sol
+++ test/MinimalDelegation.storage.t.sol
@@ -19,4 +19,14 @@ contract MinimalDelegationStorageTest is DelegationHandler {
         bytes32 customStorageRoot = keccak256(abi.encode(uint256(keccak256("Uniswap.ERC7201.1.0.0")) - 1)) & ~bytes32(uint256(0xff));
         assertEq(signerAccount.CUSTOM_STORAGE_ROOT(), customStorageRoot);
     }
+
+    function test_entrypoint_isSet() public {
+        vm.record();
+        signerAccount.ENTRY_POINT();
+        (bytes32[] memory readSlots, bytes32[] memory writeSlots) = vm.accesses(address(signerAccount));
+        assertEq(readSlots.length, 1);
+        assertEq(writeSlots.length, 0);
+        // Doesn't work because entrypoint is located at slot 4 of the storage layout
+        assertEq(readSlots[0], signerAccount.CUSTOM_STORAGE_ROOT());
+    }
 }
