14d422f132f9496904116b8ffd1686874f0cbcb4
Eric Zhong <eric.zhong@uniswap.org>
2025-04-04 19:46:30 -0400

    Cleanup

--- test/MinimalDelegation.execute.invariant.t.sol
+++ test/MinimalDelegation.execute.invariant.t.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.23;
 
-import {console2} from "forge-std/console2.sol";
 import {ERC20} from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 import {EnumerableSetLib} from "solady/utils/EnumerableSetLib.sol";
 import {ERC20Mock} from "openzeppelin-contracts/contracts/mocks/token/ERC20Mock.sol";
@@ -20,6 +19,7 @@ import {SignedCalls, SignedCallsLib} from "../src/libraries/SignedCallsLib.sol";
 import {KeyType, Key, KeyLib} from "../src/libraries/KeyLib.sol";
 import {CallBuilder} from "./utils/CallBuilder.sol";
 import {WrappedDataHash} from "../src/libraries/WrappedDataHash.sol";
+import {HandlerCall, HandlerCallLib} from "./utils/HandlerCallLib.sol";
 
 struct SetupParams {
     IMinimalDelegation _signerAccount;
@@ -27,48 +27,6 @@ struct SetupParams {
     address[] _callers;
     address _tokenA;
     address _tokenB;
-}
-
-struct HandlerCall {
-    Call call;
-    bytes callback;
-}
-
-library HandlerCallLib {
-    function init() internal pure returns (HandlerCall[] memory) {
-        return new HandlerCall[](0);
-    }
-
-    function push(HandlerCall[] memory handlerCalls, HandlerCall memory handlerCall)
-        internal
-        pure
-        returns (HandlerCall[] memory)
-    {
-        HandlerCall[] memory newCalls = new HandlerCall[](handlerCalls.length + 1);
-        for (uint256 i = 0; i < handlerCalls.length; i++) {
-            newCalls[i] = handlerCalls[i];
-        }
-        newCalls[handlerCalls.length] = handlerCall;
-        return newCalls;
-    }
-
-    function initDefault() internal pure returns (HandlerCall memory) {
-        return HandlerCall({call: Call({to: address(0), value: 0, data: ""}), callback: ""});
-    }
-
-    function withCall(HandlerCall memory handlerCall, Call memory call) internal pure returns (HandlerCall memory) {
-        handlerCall.call = call;
-        return handlerCall;
-    }
-
-    function withCallback(HandlerCall memory handlerCall, bytes memory callback)
-        internal
-        pure
-        returns (HandlerCall memory)
-    {
-        handlerCall.callback = callback;
-        return handlerCall;
-    }
 }
 
 interface IHandlerGhostCallbacks {
@@ -94,7 +52,7 @@ contract MinimalDelegationExecuteInvariantHandler is Test, ExecuteHandler {
     TestKey[] public keys;
     TestKey public currentSigningKey;
 
-    // Ghost variables
+    // Ghost variables to track registered keys
     EnumerableSetLib.Bytes32Set internal _ghostKeyHashes;
 
     ERC20Mock public tokenA;
@@ -110,15 +68,13 @@ contract MinimalDelegationExecuteInvariantHandler is Test, ExecuteHandler {
         tokenB = ERC20Mock(_params._tokenB);
     }
 
-    /// Used to switch between which keys sign data
+    /// @notice Sets the current signing key for the test
     modifier useSigningKey(uint256 keyIndexSeed) {
         currentSigningKey = keys[bound(keyIndexSeed, 0, keys.length - 1)];
-        console2.log("currentSigningKey publicKey");
-        console2.logBytes(currentSigningKey.publicKey);
         _;
     }
 
-    /// Used to switch between callers of the signerAccount
+    /// @notice Sets the current caller for the test
     modifier useCaller(uint256 callerIndexSeed) {
         currentCaller = callers[bound(callerIndexSeed, 0, callers.length - 1)];
         vm.startPrank(currentCaller);
@@ -126,33 +82,39 @@ contract MinimalDelegationExecuteInvariantHandler is Test, ExecuteHandler {
         vm.stopPrank();
     }
 
+    /// @notice Bounds call value to the account balance
     function _boundCall(Call memory call) internal view returns (Call memory) {
         call.value = bound(call.value, 0, address(signerAccount).balance);
         return call;
     }
 
+    /// @notice Selects a random element from an array
     function _randFromArray(bytes32[] memory array) internal view returns (bytes32) {
-        return array[bound(uint256(array.length), 0, array.length - 1)];
+        return array[bound(uint256(0), 0, array.length - 1)];
     }
 
+    /// @notice Selects a random element from an array
     function _randFromArray(Key[] memory array) internal view returns (Key memory) {
-        return array[bound(uint256(array.length), 0, array.length - 1)];
+        return array[bound(uint256(0), 0, array.length - 1)];
     }
 
+    /// @notice Selects a random element from an array
     function _randFromArray(HandlerCall[] memory array) internal view returns (HandlerCall memory) {
-        return array[bound(uint256(array.length), 0, array.length - 1)];
+        return array[bound(uint256(0), 0, array.length - 1)];
     }
 
-    /// Helper function to get the next available nonce
+    /// @notice Builds the next valid nonce for the given key
     function _buildNextValidNonce(uint256 key) internal view returns (uint256 nonce, uint64 seq) {
         seq = uint64(signerAccount.getSeq(key));
         nonce = key << 64 | seq;
     }
 
+    /// @notice Checks if the signing key is the root EOA
     function _signingKeyIsRootEOA(TestKey memory key) internal view returns (bool) {
         return vm.addr(key.privateKey) == address(signerAccount);
     }
 
+    /// @notice Executes registered callbacks for handler calls
     function _doCallbacks(HandlerCall[] memory handlerCalls) internal {
         for (uint256 i = 0; i < handlerCalls.length; i++) {
             if (handlerCalls[i].callback.length > 0) {
@@ -162,10 +124,12 @@ contract MinimalDelegationExecuteInvariantHandler is Test, ExecuteHandler {
         }
     }
 
+    /// @notice Ghost callback to track registered keys
     function ghost_RegisterCallback(Key memory key) public {
         _ghostKeyHashes.add(key.hash());
     }
 
+    /// @notice Creates handler calls for the test
     function _getHandlerCalls() internal returns (HandlerCall[] memory) {
         HandlerCall[] memory handlerCalls = HandlerCallLib.init();
 
@@ -198,7 +162,7 @@ contract MinimalDelegationExecuteInvariantHandler is Test, ExecuteHandler {
         return handlerCalls;
     }
 
-    // Load keys into memory
+    /// @notice Loads keys into memory
     function _getKeys() internal view returns (Key[] memory) {
         Key[] memory _keys = new Key[](keys.length);
         for (uint256 i = 0; i < keys.length; i++) {
@@ -207,7 +171,7 @@ contract MinimalDelegationExecuteInvariantHandler is Test, ExecuteHandler {
         return _keys;
     }
 
-    // TODO: support execution type
+    /// @notice Executes a batched call with the current caller
     function executeBatchedCall(uint256 callerIndexSeed) public useCaller(callerIndexSeed) {
         HandlerCall[] memory handlerCalls = _getHandlerCalls();
         HandlerCall memory handlerCall = _randFromArray(handlerCalls);
@@ -216,7 +180,6 @@ contract MinimalDelegationExecuteInvariantHandler is Test, ExecuteHandler {
         calls[0] = handlerCall.call;
 
         try signerAccount.execute(BATCHED_CALL, abi.encode(calls)) {
-            // TODO: remove once we do multiple calls
             HandlerCall[] memory _callbacks = new HandlerCall[](1);
             _callbacks[0] = handlerCall;
             _doCallbacks(_callbacks);
@@ -229,8 +192,7 @@ contract MinimalDelegationExecuteInvariantHandler is Test, ExecuteHandler {
         }
     }
 
-    /// Computes digest and signs correctly, nonce is also always fetched so should be correct
-    // TODO: add switch on caller for entrypoint vs. native bundler use case
+    /// @notice Executes a call with operation data (with signature)
     function executeWithOpData(uint192 nonceKey, uint256 keyIndexSeed) public useSigningKey(keyIndexSeed) {
         HandlerCall[] memory handlerCalls = _getHandlerCalls();
         HandlerCall memory handlerCall = _randFromArray(handlerCalls);
@@ -253,7 +215,6 @@ contract MinimalDelegationExecuteInvariantHandler is Test, ExecuteHandler {
         bool keyExists = _ghostKeyHashes.contains(currentKeyHash);
 
         try signerAccount.execute(BATCHED_CALL_SUPPORTS_OPDATA, abi.encode(calls, opData)) {
-            // TODO: remove once we do multiple calls
             HandlerCall[] memory _callbacks = new HandlerCall[](1);
             _callbacks[0] = handlerCall;
             _doCallbacks(_callbacks);
@@ -283,7 +244,7 @@ contract MinimalDelegationExecuteInvariantTest is TokenHandler, DelegationHandle
     address public untrustedCaller = makeAddr("untrustedCaller");
     uint256 public untrustedPrivateKey = 0xdead;
 
-    // address who is calling the invariantHandler, shouldn't matter
+    // Address calling the invariantHandler
     address internal sender = makeAddr("sender");
 
     TestKey[] internal _keys;
@@ -326,6 +287,7 @@ contract MinimalDelegationExecuteInvariantTest is TokenHandler, DelegationHandle
         excludeSelector(selector);
     }
 
+    /// @notice Verifies that the root key can always revoke other keys
     function invariant_rootKeyCanAlwaysRevokeOtherKeys() public {
         // Ensure key exists
         bytes32 keyHash = untrustedKey.toKeyHash();
---
+++ test/utils/HandlerCallLib.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.23;
+
+import {Call} from "../../src/libraries/CallLib.sol";
+
+struct HandlerCall {
+    Call call;
+    bytes callback;
+}
+
+library HandlerCallLib {
+    function init() internal pure returns (HandlerCall[] memory) {
+        return new HandlerCall[](0);
+    }
+
+    function push(HandlerCall[] memory handlerCalls, HandlerCall memory handlerCall)
+        internal
+        pure
+        returns (HandlerCall[] memory)
+    {
+        HandlerCall[] memory newCalls = new HandlerCall[](handlerCalls.length + 1);
+        for (uint256 i = 0; i < handlerCalls.length; i++) {
+            newCalls[i] = handlerCalls[i];
+        }
+        newCalls[handlerCalls.length] = handlerCall;
+        return newCalls;
+    }
+
+    function initDefault() internal pure returns (HandlerCall memory) {
+        return HandlerCall({call: Call({to: address(0), value: 0, data: ""}), callback: ""});
+    }
+
+    function withCall(HandlerCall memory handlerCall, Call memory call) internal pure returns (HandlerCall memory) {
+        handlerCall.call = call;
+        return handlerCall;
+    }
+
+    function withCallback(HandlerCall memory handlerCall, bytes memory callback)
+        internal
+        pure
+        returns (HandlerCall memory)
+    {
+        handlerCall.callback = callback;
+        return handlerCall;
+    }
+}
