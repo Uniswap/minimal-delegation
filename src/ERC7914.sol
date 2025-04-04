e751db313e9aa9560f0ae414da8bf2a5eed80a9f
dianakocsis <diana.kocsis@uniswap.org>
2025-04-04 16:39:23 -0400

    remove storage lib

--- src/ERC7914.sol
+++ src/ERC7914.sol
@@ -2,19 +2,15 @@
 pragma solidity ^0.8.23;
 
 import {IERC7914} from "./interfaces/IERC7914.sol";
-import {MinimalDelegationStorage, MinimalDelegationStorageLib} from "./libraries/MinimalDelegationStorage.sol";
 import {TransientAllowance} from "./libraries/TransientAllowance.sol";
 
 /// @title ERC-7914
 /// @notice Abstract ERC-7914 implementation
 abstract contract ERC7914 is IERC7914 {
+    mapping(address => uint256) public allowance;
+
     /// @dev Must be overridden by the implementation
     function _onlyThis() internal view virtual {}
-
-    /// @inheritdoc IERC7914
-    function allowance(address spender) external view override returns (uint256) {
-        return MinimalDelegationStorageLib.get().allowance[spender];
-    }
 
     /// @inheritdoc IERC7914
     function transientAllowance(address spender) external view returns (uint256) {
@@ -24,7 +20,7 @@ abstract contract ERC7914 is IERC7914 {
     /// @inheritdoc IERC7914
     function approveNative(address spender, uint256 amount) external override returns (bool) {
         _onlyThis();
-        MinimalDelegationStorageLib.get().allowance[spender] = amount;
+        allowance[spender] = amount;
         emit ApproveNative(address(this), spender, amount);
         return true;
     }
@@ -32,9 +28,9 @@ abstract contract ERC7914 is IERC7914 {
     /// @inheritdoc IERC7914
     function transferFromNative(address from, address recipient, uint256 amount) external override returns (bool) {
         if (from != address(this)) revert IncorrectSender();
-        if (MinimalDelegationStorageLib.get().allowance[msg.sender] < amount) revert AllowanceExceeded();
+        if (allowance[msg.sender] < amount) revert AllowanceExceeded();
         if (amount == 0) return false; // early return for amount == 0
-        MinimalDelegationStorageLib.get().allowance[msg.sender] -= amount;
+        allowance[msg.sender] -= amount;
         (bool success,) = payable(recipient).call{value: amount}("");
         if (!success) {
             revert TransferNativeFailed();
--- test/MinimalDelegation.storage.t.sol
+++ test/MinimalDelegation.storage.t.sol
@@ -9,19 +9,21 @@ contract MinimalDelegationStorageTest is DelegationHandler {
      * MinimalDelegation storage layout
      * slots are assigned starting from the custom layout slot and in order of declaration, from left to right
      *
-     * MinimalDelegation is IERC7821, ERC1271, EIP712, ERC4337Account, Receiver, KeyManagement, NonceManager, ERC7201 layout at 0xc807f46cbe2302f9a007e47db23c8af6a94680c1d26280fb9582873dbe5c9200
+     * MinimalDelegation is IERC7821, ERC1271, EIP712, ERC4337Account, Receiver, KeyManagement, NonceManager, ERC7914, ERC7201 layout at 0xc807f46cbe2302f9a007e47db23c8af6a94680c1d26280fb9582873dbe5c9200
      *
      * 0: mapping(bytes32 keyHash => KeyExtraStorage) keyExtraStorage;
      * 1: mapping(bytes32 keyHash => bytes encodedKey) keyStorage;
      * 2: EnumerableSetLib.Bytes32Set keyHashes;
      * 3: mapping(uint256 key => uint256 seq) nonceSequenceNumber
-     * 4: uint256 entryPoint
+     * 4: mapping(address => uint256) allowance;
+     * 5: uint256 entryPoint
      */
     uint256 private constant KEY_EXTRA_STORAGE_SLOT = 0;
     uint256 private constant KEY_STORAGE_SLOT = 1;
     uint256 private constant KEY_HASHES_SLOT = 2;
     uint256 private constant NONCE_SEQUENCE_NUMBER_SLOT = 3;
-    uint256 private constant ENTRY_POINT_SLOT = 4;
+    uint256 private constant ALLOWANCE_SLOT = 4;
+    uint256 private constant ENTRY_POINT_SLOT = 5;
 
     function setUp() public {
         setUpDelegation();
@@ -35,13 +37,17 @@ contract MinimalDelegationStorageTest is DelegationHandler {
         return keccak256(abi.encode(key, uint256(rootSlot)));
     }
 
+    function _calculateNestedMappingSlotAddress(address addr, bytes32 rootSlot) internal pure returns (bytes32) {
+        return keccak256(abi.encode(addr, uint256(rootSlot)));
+    }
+
     /// @dev Sanity check tests for changes in namespace and version
-    function test_erc7201_namespaceAndVersion() public {
+    function test_erc7201_namespaceAndVersion() public view {
         assertEq(signerAccount.namespaceAndVersion(), "Uniswap.MinimalDelegation.1.0.0");
     }
 
     /// @dev Sanity check tests for changes in the calculated custom storage root
-    function test_erc7201_customStorageRoot() public {
+    function test_erc7201_customStorageRoot() public view {
         bytes32 customStorageRoot =
             keccak256(abi.encode(uint256(keccak256("Uniswap.MinimalDelegation.1.0.0")) - 1)) & ~bytes32(uint256(0xff));
         assertEq(signerAccount.CUSTOM_STORAGE_ROOT(), customStorageRoot);
@@ -58,6 +64,18 @@ contract MinimalDelegationStorageTest is DelegationHandler {
 
         bytes32 mappingRootSlot = _addOffset(signerAccount.CUSTOM_STORAGE_ROOT(), NONCE_SEQUENCE_NUMBER_SLOT);
         bytes32 nestedSlot = _calculateNestedMappingSlot(key, mappingRootSlot);
+        assertEq(readSlots[0], nestedSlot);
+    }
+
+    function test_allowance() public {
+        vm.record();
+        signerAccount.allowance(address(0));
+        (bytes32[] memory readSlots, bytes32[] memory writeSlots) = vm.accesses(address(signerAccount));
+        assertEq(readSlots.length, 1);
+        assertEq(writeSlots.length, 0);
+
+        bytes32 mappingRootSlot = _addOffset(signerAccount.CUSTOM_STORAGE_ROOT(), ALLOWANCE_SLOT);
+        bytes32 nestedSlot = _calculateNestedMappingSlotAddress(address(0), mappingRootSlot);
         assertEq(readSlots[0], nestedSlot);
     }
 
