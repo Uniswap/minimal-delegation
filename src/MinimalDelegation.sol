03915a6c5c29e154e0467aebc71584483c8ee424
Eric Zhong <eric.zhong@uniswap.org>
2025-04-04 11:31:20 -0400

    nit ordering

--- src/MinimalDelegation.sol
+++ src/MinimalDelegation.sol
@@ -79,6 +79,7 @@ contract MinimalDelegation is IERC7821, ERC1271, EIP712, ERC4337Account, Receive
         (uint256 nonce, bytes calldata wrappedSignature) = opData.decodeUint256Bytes();
         _useNonce(nonce);
         ExecutionData memory executionData = ExecutionData({calls: calls, nonce: nonce});
+        bytes32 digest = _hashTypedData(executionData.hash());
 
         (bytes32 keyHash, bytes calldata signature) = wrappedSignature.unwrap();
         Key memory key = _getKey(keyHash);
@@ -86,8 +87,6 @@ contract MinimalDelegation is IERC7821, ERC1271, EIP712, ERC4337Account, Receive
         IHook hook = MinimalDelegationStorageLib.get().keySettings[keyHash].hook();
 
         /// TODO: Handle key expiry check.
-        bytes32 digest = _hashTypedData(executionData.hash());
-
         bool isValid = hook.hasPermission(HooksLib.VERIFY_SIGNATURE_FLAG)
             ? hook.verifySignature(keyHash, digest, signature)
             : key.verify(digest, signature);
