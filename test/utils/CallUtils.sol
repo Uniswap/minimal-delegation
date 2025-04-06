a4c13a628a339823c6f0fe9d22a3a74e43b43e05
Eric Zhong <eric.zhong@uniswap.org>
2025-04-06 16:40:53 -0400

    simplify

--- test/utils/CallUtils.sol
+++ test/utils/CallUtils.sol
@@ -7,19 +7,13 @@ import {IERC7821} from "../../src/interfaces/IERC7821.sol";
 import {Settings} from "../../src/libraries/SettingsLib.sol";
 import {TestKeyManager, TestKey} from "./TestKeyManager.sol";
 
-/**
- * @title HandlerCall
- * @dev A wrapper around Call that includes callback data for processing after execution
- */
+/// @dev A wrapper around Call that includes callback data for processing after execution
 struct HandlerCall {
     Call call;
     bytes callback;
 }
 
-/**
- * @title CallUtils
- * @dev Consolidated utility library for creating, manipulating, and encoding Call objects for testing
- */
+/// @dev Utility library for Call and HandlerCall objects
 library CallUtils {
     using CallUtils for Call;
     using CallUtils for Call[];
@@ -31,23 +25,14 @@ library CallUtils {
     address constant SELF_CALL = address(0);
     bytes32 internal constant BATCHED_CALL = 0x0100000000000000000000000000000000000000000000000000000000000000;
 
-    //--------------------------------------------------------------------------------------//
     // Call array operations
-    //--------------------------------------------------------------------------------------//
 
-    /**
-     * @notice Initialize an empty Call array
-     */
+    /// @dev Create empty Call array
     function init() internal pure returns (Call[] memory) {
         return new Call[](0);
     }
 
-    /**
-     * @notice Add a call to a call array
-     * @param calls Existing call array
-     * @param call Call to add
-     * @return New call array with the added call
-     */
+    /// @dev Add a call to an array
     function push(Call[] memory calls, Call memory call) internal pure returns (Call[] memory) {
         Call[] memory newCalls = new Call[](calls.length + 1);
         for (uint256 i = 0; i < calls.length; i++) {
@@ -57,145 +42,86 @@ library CallUtils {
         return newCalls;
     }
 
-    //--------------------------------------------------------------------------------------//
     // Call manipulation
-    //--------------------------------------------------------------------------------------//
 
-    /**
-     * @notice Initialize a default Call object
-     */
+    /// @dev Create default empty Call
     function initDefault() internal pure returns (Call memory) {
         return Call({to: address(0), value: 0, data: ""});
     }
 
-    /**
-     * @notice Set the 'to' field of a Call
-     * @param call Call to modify
-     * @param to Target address
-     */
     function withTo(Call memory call, address to) internal pure returns (Call memory) {
         call.to = to;
         return call;
     }
 
-    /**
-     * @notice Set the 'value' field of a Call
-     * @param call Call to modify
-     * @param value ETH value to send
-     */
     function withValue(Call memory call, uint256 value) internal pure returns (Call memory) {
         call.value = value;
         return call;
     }
 
-    /**
-     * @notice Set the 'data' field of a Call
-     * @param call Call to modify
-     * @param data Calldata to include
-     */
     function withData(Call memory call, bytes memory data) internal pure returns (Call memory) {
         call.data = data;
         return call;
     }
 
-    //--------------------------------------------------------------------------------------//
     // Call encoding for specific operations
-    //--------------------------------------------------------------------------------------//
 
-    /**
-     * @notice Add an execute call to an existing call array
-     * @param calls Existing call array
-     * @param innerCalls Calls to execute
-     */
+    /// @dev Add execute call to array
     function addExecute(Call[] memory calls, Call[] memory innerCalls) internal pure returns (Call[] memory) {
         return push(calls, encodeExecuteCall(innerCalls));
     }
 
-    /**
-     * @notice Create a call that will execute a batch of calls
-     * @param calls Calls to execute
-     */
+    /// @dev Create call to execute a batch
     function encodeExecuteCall(Call[] memory calls) internal pure returns (Call memory) {
         return initDefault().withTo(SELF_CALL).withData(
             abi.encodeWithSelector(IERC7821.execute.selector, BATCHED_CALL, abi.encode(calls))
         );
     }
 
-    /**
-     * @notice Add a register key call to an existing call array
-     * @param calls Existing call array
-     * @param newKey Key to register
-     */
+    /// @dev Add register key call
     function addRegister(Call[] memory calls, TestKey memory newKey) internal pure returns (Call[] memory) {
         return push(calls, encodeRegisterCall(newKey));
     }
 
-    /**
-     * @notice Create a call that will register a key
-     * @param newKey Key to register
-     */
+    /// @dev Create call to register key
     function encodeRegisterCall(TestKey memory newKey) internal pure returns (Call memory) {
         return initDefault().withTo(SELF_CALL).withData(
             abi.encodeWithSelector(IKeyManagement.register.selector, newKey.toKey())
         );
     }
 
-    /**
-     * @notice Add a revoke key call to an existing call array
-     * @param calls Existing call array
-     * @param keyHash Hash of key to revoke
-     */
+    /// @dev Add revoke key call
     function addRevoke(Call[] memory calls, bytes32 keyHash) internal pure returns (Call[] memory) {
         return push(calls, encodeRevokeCall(keyHash));
     }
 
-    /**
-     * @notice Create a call that will revoke a key
-     * @param keyHash Hash of key to revoke
-     */
+    /// @dev Create call to revoke key
     function encodeRevokeCall(bytes32 keyHash) internal pure returns (Call memory) {
         return initDefault().withTo(SELF_CALL).withData(
             abi.encodeWithSelector(IKeyManagement.revoke.selector, keyHash)
         );
     }
 
-    /**
-     * @notice Add an update key settings call to an existing call array
-     * @param calls Existing call array
-     * @param keyHash Hash of key to update
-     * @param settings New settings for the key
-     */
+    /// @dev Add update key settings call
     function addUpdate(Call[] memory calls, bytes32 keyHash, Settings settings) internal pure returns (Call[] memory) {
         return push(calls, encodeUpdateCall(keyHash, settings));
     }
 
-    /**
-     * @notice Create a call that will update key settings
-     * @param keyHash Hash of key to update
-     * @param settings New settings for the key
-     */
+    /// @dev Create call to update key settings
     function encodeUpdateCall(bytes32 keyHash, Settings settings) internal pure returns (Call memory) {
         return initDefault().withTo(SELF_CALL).withData(
             abi.encodeWithSelector(IKeyManagement.update.selector, keyHash, settings)
         );
     }
 
-    //--------------------------------------------------------------------------------------//
     // HandlerCall operations
-    //--------------------------------------------------------------------------------------//
 
-    /**
-     * @notice Initialize an empty HandlerCall array
-     */
+    /// @dev Create empty HandlerCall array
     function initHandler() internal pure returns (HandlerCall[] memory) {
         return new HandlerCall[](0);
     }
 
-    /**
-     * @notice Add a HandlerCall to a HandlerCall array
-     * @param handlerCalls Existing HandlerCall array
-     * @param handlerCall HandlerCall to add
-     */
+    /// @dev Add a HandlerCall to array
     function push(HandlerCall[] memory handlerCalls, HandlerCall memory handlerCall)
         internal
         pure
@@ -209,10 +135,7 @@ library CallUtils {
         return newCalls;
     }
 
-    /**
-     * @notice Convert a HandlerCall array to a Call array
-     * @param handlerCalls HandlerCall array to convert
-     */
+    /// @dev Convert HandlerCall array to Call array
     function toCalls(HandlerCall[] memory handlerCalls) internal pure returns (Call[] memory) {
         Call[] memory calls = new Call[](handlerCalls.length);
         for (uint256 i = 0; i < handlerCalls.length; i++) {
@@ -221,28 +144,16 @@ library CallUtils {
         return calls;
     }
 
-    /**
-     * @notice Initialize a default HandlerCall
-     */
+    /// @dev Create default empty HandlerCall
     function initHandlerDefault() internal pure returns (HandlerCall memory) {
         return HandlerCall({call: Call({to: address(0), value: 0, data: ""}), callback: ""});
     }
 
-    /**
-     * @notice Set the 'call' field of a HandlerCall
-     * @param handlerCall HandlerCall to modify
-     * @param call Call to set
-     */
     function withCall(HandlerCall memory handlerCall, Call memory call) internal pure returns (HandlerCall memory) {
         handlerCall.call = call;
         return handlerCall;
     }
 
-    /**
-     * @notice Set the 'callback' field of a HandlerCall
-     * @param handlerCall HandlerCall to modify
-     * @param callback Callback data to set
-     */
     function withCallback(HandlerCall memory handlerCall, bytes memory callback)
         internal
         pure
