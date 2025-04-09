7f56d09ff5fcb544ddf1f72bff7551154492ab0d
dianakocsis <diana.kocsis@uniswap.org>
2025-04-09 13:12:47 -0400

    comment

--- src/libraries/TransientAllowance.sol
+++ src/libraries/TransientAllowance.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.8.24;
 
 /// @notice This is a temporary library that allows us to use transient storage (tstore/tload)
 /// TODO: This library can be deleted when we have the transient keyword support in solidity.
+/// @dev The custom storage layout keyword does not work for transient storage
+/// since transient storage is automatically cleared between transactions and does not persist
 library TransientAllowance {
     /// @notice calculates which storage slot a transient allowance should be stored in for a given spender
     function _computeSlot(address spender) internal pure returns (bytes32 hashSlot) {
