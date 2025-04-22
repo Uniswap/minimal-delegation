3c7345081e47c9ae0e06939006173ddf236b7125
Eric Zhong <ezhong1900@gmail.com>
2025-04-22 01:19:09 -0400

    rename (#158)

--- src/libraries/SignedBatchedCallLib.sol
+++ src/libraries/SignedBatchedCallLib.sol
@@ -17,7 +17,7 @@ library SignedBatchedCallLib {
 
     /// @dev The type string for the SignedBatchedCall struct
     bytes internal constant SIGNED_BATCHED_CALL_TYPE =
-        "SignedBatchedCall(BatchedCall batchedCall,uint256 nonce,bytes32 keyHash,address executor)BatchedCall(Call[] calls,bool shouldRevert)Call(address to,uint256 value,bytes data)";
+        "SignedBatchedCall(BatchedCall batchedCall,uint256 nonce,bytes32 keyHash,address executor)BatchedCall(Call[] calls,bool revertOnFailure)Call(address to,uint256 value,bytes data)";
     /// @dev The typehash for the SignedBatchedCall struct
     bytes32 internal constant SIGNED_BATCHED_CALL_TYPEHASH = keccak256(SIGNED_BATCHED_CALL_TYPE);
 
