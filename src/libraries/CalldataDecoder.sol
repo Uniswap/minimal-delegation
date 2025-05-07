20991e4aabedfeab72046cbaaf3f71c861655b64
Sara Reynolds <snreynolds2506@gmail.com>
2025-05-07 17:01:05 -0400

    fix: add length check

--- src/libraries/CalldataDecoder.sol
+++ src/libraries/CalldataDecoder.sol
@@ -1,19 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.23;
 
-import "forge-std/console2.sol";
-
 /// @title CalldataDecoder
 library CalldataDecoder {
+    /// error SliceOutOfBounds();
+    uint256 constant SLICE_ERROR_SELECTOR = 0x3b99b53d;
+
     /// @notice Removes the selector from the calldata and returns the encoded params.
     function removeSelector(bytes calldata data) internal pure returns (bytes calldata params) {
-        uint256 length;
         assembly {
-            length := data.length
+            if lt(data.length, 4) {
+                mstore(0, SLICE_ERROR_SELECTOR)
+                revert(0x1c, 4)
+            }
             params.offset := add(data.offset, 4)
             params.length := sub(data.length, 4)
         }
-
-        console2.log("length", length);
     }
 }
