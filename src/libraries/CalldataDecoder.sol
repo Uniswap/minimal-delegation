88db6a1e0cd7703dc79af6865e4e97fa7f19bc1b
Sara Reynolds <snreynolds2506@gmail.com>
2025-05-07 13:21:34 -0400

    add test

--- src/libraries/CalldataDecoder.sol
+++ src/libraries/CalldataDecoder.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.23;
 
+import "forge-std/console2.sol";
+
 /// @title CalldataDecoder
 library CalldataDecoder {
     using CalldataDecoder for bytes;
@@ -14,10 +16,14 @@ library CalldataDecoder {
 
     /// @notice Removes the selector from the calldata and returns the encoded params.
     function removeSelector(bytes calldata data) internal pure returns (bytes calldata params) {
+        uint256 length;
         assembly ("memory-safe") {
+            length := data.length
             params.offset := add(data.offset, 4)
             params.length := sub(data.length, 4)
         }
+
+        console2.log("length", length);
     }
 
     /// @notice Decode the signature and hook data from the calldata
---
+++ test/CalldataDecoder.t.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+import {Test} from "forge-std/Test.sol";
+import {Call} from "../src/libraries/CallLib.sol";
+import {CalldataDecoder} from "../src/libraries/CalldataDecoder.sol";
+import {MockCalldataDecoder} from "./utils/MockCalldataDecoder.sol";
+
+contract CalldataDecoderTest is Test {
+    using CalldataDecoder for bytes;
+
+    MockCalldataDecoder decoder;
+
+    function setUp() public {
+        decoder = new MockCalldataDecoder();
+    }
+
+    function test_removeSelector() public view {
+        bytes4 selector = bytes4(keccak256("test"));
+        bytes memory data = abi.encodeWithSelector(selector, uint256(1), uint256(2));
+        bytes memory dataWithoutSelector = decoder.removeSelector(data);
+
+        (uint256 one, uint256 two) = abi.decode(dataWithoutSelector, (uint256, uint256));
+        assertEq(one, 1);
+        assertEq(two, 2);
+    }
+
+    function test_removeSelector_lessThan4Bytes() public view {
+        bytes memory selector = abi.encode(0x4e);
+        decoder.removeSelector(selector);
+    }
+}
