b6708006202d9d656b4c9753dfd122fabbc3f695
Eric Zhong <eric.zhong@uniswap.org>
2025-05-10 16:55:52 -0400

    Add basic fuzz tests for KeyLib

--- test/libraries/KeyLib.t.sol
+++ test/libraries/KeyLib.t.sol
@@ -4,9 +4,11 @@ pragma solidity ^0.8.23;
 import {Test} from "forge-std/Test.sol";
 import {KeyType, Key, KeyLib} from "../../src/libraries/KeyLib.sol";
 import {MockKeyLib} from "../utils/MockKeyLib.sol";
+import {TestKey, TestKeyManager} from "../utils/TestKeyManager.sol";
 
 contract KeyLibTest is Test {
     using KeyLib for Key;
+    using TestKeyManager for TestKey;
 
     MockKeyLib mockKeyLib;
 
@@ -42,5 +44,56 @@ contract KeyLibTest is Test {
                 KeyLib.hash(Key({keyType: KeyType.Secp256k1, publicKey: abi.encode(caller)}))
             );
         }
+    }
+
+    function test_verify_secp256k1_compactSignature_valid_fuzz(bytes32 digest) public view {
+        TestKey memory testKey = TestKeyManager.initDefault(KeyType.Secp256k1);
+        (bytes32 r, bytes32 vs) = vm.signCompact(testKey.privateKey, digest);
+        bytes memory signature = abi.encodePacked(r, vs);
+        assertEq(KeyLib.verify(testKey.toKey(), digest, signature), true);
+    }
+
+    function test_verify_secp256k1_compactSignature_invalid_fuzz(bytes32 digest) public view {
+        bytes32 invalidDigest = keccak256(abi.encode(digest));
+        TestKey memory testKey = TestKeyManager.initDefault(KeyType.Secp256k1);
+        // Sign the invalid digest
+        (bytes32 r, bytes32 vs) = vm.signCompact(testKey.privateKey, invalidDigest);
+        bytes memory signature = abi.encodePacked(r, vs);
+        // Try to verify against the original digest
+        assertEq(KeyLib.verify(testKey.toKey(), digest, signature), false);
+    }
+
+    function test_verify_secp256k1_signature_valid_fuzz(bytes32 digest) public view {
+        TestKey memory testKey = TestKeyManager.initDefault(KeyType.Secp256k1);
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(testKey.privateKey, digest);
+        bytes memory signature = abi.encodePacked(r, s, v);
+        assertEq(KeyLib.verify(testKey.toKey(), digest, signature), true);
+    }
+
+    function test_verify_secp256k1_signature_invalid_fuzz(bytes32 digest) public view {
+        bytes32 invalidDigest = keccak256(abi.encode(digest));
+        TestKey memory testKey = TestKeyManager.initDefault(KeyType.Secp256k1);
+        // Sign the invalid digest
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(testKey.privateKey, invalidDigest);
+        bytes memory signature = abi.encodePacked(r, s, v);
+        // Try to verify against the original digest
+        assertEq(KeyLib.verify(testKey.toKey(), digest, signature), false);
+    }
+
+    function test_verify_p256_signature_valid_fuzz(bytes32 digest) public view {
+        TestKey memory testKey = TestKeyManager.initDefault(KeyType.P256);
+        (bytes32 r, bytes32 s) = vm.signP256(testKey.privateKey, digest);
+        bytes memory signature = abi.encodePacked(r, s);
+        assertEq(KeyLib.verify(testKey.toKey(), digest, signature), true);
+    }
+
+    function test_verify_p256_signature_invalid_fuzz(bytes32 digest) public view {
+        bytes32 invalidDigest = keccak256(abi.encode(digest));
+        TestKey memory testKey = TestKeyManager.initDefault(KeyType.P256);
+        // Sign the invalid digest
+        (bytes32 r, bytes32 s) = vm.signP256(testKey.privateKey, invalidDigest);
+        bytes memory signature = abi.encodePacked(r, s);
+        // Try to verify against the original digest
+        assertEq(KeyLib.verify(testKey.toKey(), digest, signature), false);
     }
 }
