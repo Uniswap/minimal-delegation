28cc1d213ef1fbdb96ebee601d6c7bc3e8640e2c
Eric Zhong <eric.zhong@uniswap.org>
2025-05-13 13:51:46 -0400

    comments

--- src/libraries/KeyLib.sol
+++ src/libraries/KeyLib.sol
@@ -61,11 +61,15 @@ library KeyLib {
         if (key.keyType == KeyType.Secp256k1) {
             isValid = ECDSA.tryRecover(_hash, signature) == abi.decode(key.publicKey, (address));
         } else if (key.keyType == KeyType.P256) {
+            // Extract x,y from the public key
             (bytes32 x, bytes32 y) = abi.decode(key.publicKey, (bytes32, bytes32));
+            // Split signature into r and s values.
             (bytes32 r, bytes32 s) = abi.decode(signature, (bytes32, bytes32));
             isValid = P256.verify(EfficientHashLib.sha2(_hash), r, s, x, y);
         } else if (key.keyType == KeyType.WebAuthnP256) {
+            // Extract x,y from the public key
             (uint256 x, uint256 y) = abi.decode(key.publicKey, (uint256, uint256));
+            // Decode the signature into the WebAuthnAuth struct
             WebAuthn.WebAuthnAuth memory auth = abi.decode(signature, (WebAuthn.WebAuthnAuth));
             isValid = WebAuthn.verify({challenge: abi.encode(_hash), requireUV: false, webAuthnAuth: auth, x: x, y: y});
         } else {
