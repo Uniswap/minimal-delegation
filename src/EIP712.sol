ef54a91d5ab01fe664783184b9a2d6e34fde937c
Eric Zhong <eric.zhong@uniswap.org>
2025-05-14 17:26:56 -0400

    feedback

--- src/EIP712.sol
+++ src/EIP712.sol
@@ -97,10 +97,10 @@ contract EIP712 is IEIP712, IERC5267, BaseAuthorization {
     /// @inheritdoc IEIP712
     function updateSalt(uint96 prefix) external onlyThis {
         if (prefix != _saltPrefix) {
+            _saltPrefix = prefix;
             // per EIP-5267, emit an event to notify that the domain separator has changed
             emit EIP712DomainChanged();
         }
-        _saltPrefix = prefix;
     }
 
     /// @notice Returns the domain name and version to use when creating EIP-712 signatures.
