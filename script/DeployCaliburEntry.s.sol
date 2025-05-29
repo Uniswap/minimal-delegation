97b34476c3df0e3edf7ce8e9df9588d7a4904904
Eric Zhong <eric.zhong@uniswap.org>
2025-05-29 15:42:28 -0400

    uncomment

--- script/DeployCaliburEntry.s.sol
+++ script/DeployCaliburEntry.s.sol
@@ -1,19 +1,19 @@
-// // SPDX-License-Identifier: UNLICENSED
-// pragma solidity ^0.8.23;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.23;
 
-// import "forge-std/console2.sol";
-// import "forge-std/Script.sol";
-// import {CaliburEntry} from "../src/CaliburEntry.sol";
+import "forge-std/console2.sol";
+import "forge-std/Script.sol";
+import {CaliburEntry} from "../src/CaliburEntry.sol";
 
-// contract DeployCaliburEntry is Script {
-//     function setUp() public {}
+contract DeployCaliburEntry is Script {
+    function setUp() public {}
 
-//     function run() public returns (CaliburEntry entry) {
-//         vm.startBroadcast();
+    function run() public returns (CaliburEntry entry) {
+        vm.startBroadcast();
 
-//         entry = new CaliburEntry{salt: bytes32(0)}();
-//         console2.log("CaliburEntry", address(entry));
+        entry = new CaliburEntry{salt: bytes32(0)}();
+        console2.log("CaliburEntry", address(entry));
 
-//         vm.stopBroadcast();
-//     }
-// }
+        vm.stopBroadcast();
+    }
+}
