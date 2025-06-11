3aedd715abc570f61e62ff880c8f9b7194000b5b
Cody Born <cody.born@uniswap.org>
2025-06-11 15:50:53 +0200

    7739 with nativeETH permit2

--- test/ERC7739.t.sol
+++ test/ERC7739.t.sol
@@ -16,6 +16,7 @@ import {Permit2Utils} from "./utils/Permit2Utils.sol";
 import {IPermit2} from "../lib/permit2/src/interfaces/IPermit2.sol";
 import {IAllowanceTransfer} from "../lib/permit2/src/interfaces/IAllowanceTransfer.sol";
 import {PermitHash} from "../lib/permit2/src/libraries/PermitHash.sol";
+import {ERC20ETH} from "../lib/erc20-eth/src/ERC20Eth.sol";
 
 contract ERC7739Test is DelegationHandler, TokenHandler, ERC1271Handler, FFISignTypedData {
     using TestKeyManager for TestKey;
@@ -220,6 +221,108 @@ contract ERC7739Test is DelegationHandler, TokenHandler, ERC1271Handler, FFISign
         
         // Verify allowance is now zero
         (uint160 finalAllowance,,) = permit2.allowance(address(signerAccount), address(tokenA), address(this));
+        assertEq(finalAllowance, 0);
+    }
+
+    function test_signTypedSignData_permitSingleTransfer_transferNative() public {
+        // Create a test key with a different private key and register it with the signer account
+        uint256 testPrivateKey = 0x123456;
+        TestKey memory testKey = TestKeyManager.withSeed(KeyType.Secp256k1, testPrivateKey);
+        vm.prank(address(signerAccount));
+        signerAccount.register(testKey.toKey());
+
+        // Deploy ERC20ETH contract for native ETH transfers
+        ERC20ETH erc20Eth = new ERC20ETH();
+        
+        // Give the signer account some ETH 
+        uint256 initialEthBalance = 10 ether;
+        vm.deal(address(signerAccount), initialEthBalance);
+        
+        // Approve ERC20ETH to spend native ETH on behalf of the signer account
+        vm.prank(address(signerAccount));
+        signerAccount.approveNative(address(erc20Eth), type(uint256).max);
+
+        // Create a PermitSingle for permit2 using ERC20ETH
+        uint160 allowanceAmount = 1 ether;
+        IAllowanceTransfer.PermitSingle memory permitSingle = IAllowanceTransfer.PermitSingle({
+            details: IAllowanceTransfer.PermitDetails({
+                token: address(erc20Eth), 
+                amount: allowanceAmount, 
+                expiration: uint48(block.timestamp + 1 hours), 
+                nonce: 0
+            }),
+            spender: address(this),
+            sigDeadline: block.timestamp + 1 hours
+        });
+
+        // Generate the permit2 domain separator and hash
+        bytes32 permit2DomainSeparator = permit2.DOMAIN_SEPARATOR();
+        bytes32 permitHash = permitSingle.hash();
+        
+        // Build ERC-7739 TypedDataSign signature for permit2
+        string memory contentsDescr = "PermitDetails(address token,uint160 amount,uint48 expiration,uint48 nonce)PermitSingle(PermitDetails details,address spender,uint256 sigDeadline)PermitSingle";
+        (string memory contentsName, string memory contentsType) = mockERC7739Utils.decodeContentsDescr(contentsDescr);
+
+        bytes memory signerAccountDomainBytes = IERC5267(address(signerAccount)).toDomainBytes();
+        bytes32 typedDataSignDigest = permitHash.hashTypedDataSign(signerAccountDomainBytes, permit2DomainSeparator, contentsName, contentsType);
+
+        // Sign the digest with the test key
+        bytes memory signature = testKey.sign(typedDataSignDigest);
+
+        // Build the TypedDataSign signature for ERC-7739
+        bytes memory typedDataSignSignature = TypedDataSignBuilder.buildTypedDataSignSignature(signature, permit2DomainSeparator, permitHash, contentsDescr);
+
+        // Wrap the signature with keyHash and empty hook data
+        bytes memory wrappedSignature = abi.encode(testKey.toKeyHash(), typedDataSignSignature, bytes(""));
+
+        // Test by calling permit2.permit() which will internally call isValidSignature() on the signer account
+        // This should succeed without reverting, proving the signature is valid
+        permit2.permit(address(signerAccount), permitSingle, wrappedSignature);
+        
+        // Verify the allowance was actually set
+        (uint160 allowance, uint48 expiration, uint48 nonce) = permit2.allowance(address(signerAccount), address(erc20Eth), address(this));
+        assertEq(allowance, allowanceAmount);
+        assertEq(expiration, uint48(block.timestamp + 1 hours));
+        assertEq(nonce, 1); // nonce should be incremented after permit
+
+        // Now verify the spender can actually pull the ETH
+        address recipient = address(0xBEEF);
+        uint160 transferAmount = 0.5 ether;
+        
+        // Check initial ETH balances
+        uint256 signerEthBalanceBefore = address(signerAccount).balance;
+        uint256 recipientEthBalanceBefore = recipient.balance;
+
+        // Verify wrong spender cannot transfer ETH
+        address wrongSpender = address(0xBAD);
+        vm.expectRevert(); // Should revert with insufficient allowance
+        vm.prank(wrongSpender);
+        permit2.transferFrom(address(signerAccount), wrongSpender, 1, address(erc20Eth));
+
+        // Verify correct spender cannot exceed allowance limit
+        vm.expectRevert(); // Should revert with insufficient allowance
+        permit2.transferFrom(address(signerAccount), recipient, allowanceAmount + 1, address(erc20Eth));
+        
+        // Transfer ETH using the permit
+        permit2.transferFrom(address(signerAccount), recipient, transferAmount, address(erc20Eth));
+        
+        // Verify ETH balances changed correctly
+        assertEq(address(signerAccount).balance, signerEthBalanceBefore - transferAmount);
+        assertEq(recipient.balance, recipientEthBalanceBefore + transferAmount);
+        
+        // Verify allowance was decremented
+        (uint160 allowanceAfter,,) = permit2.allowance(address(signerAccount), address(erc20Eth), address(this));
+        assertEq(allowanceAfter, allowanceAmount - transferAmount);
+        
+        // Transfer remaining allowed ETH
+        permit2.transferFrom(address(signerAccount), recipient, allowanceAmount - transferAmount, address(erc20Eth));
+        
+        // Verify final ETH balances
+        assertEq(address(signerAccount).balance, initialEthBalance - allowanceAmount);
+        assertEq(recipient.balance, allowanceAmount);
+        
+        // Verify allowance is now zero
+        (uint160 finalAllowance,,) = permit2.allowance(address(signerAccount), address(erc20Eth), address(this));
         assertEq(finalAllowance, 0);
     }
 }
